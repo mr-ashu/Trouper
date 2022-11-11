0c27bade8b35d5d883d24846acaeb6c7875a267c
Darshan Kale <darshankale52@gmail.com>
2022-11-12 00:29:51 +0530

    payment half

--- Trouper.com/frontend/src/App.js
+++ Trouper.com/frontend/src/App.js
@@ -4,6 +4,8 @@ import { Navbar } from './Components/Navbar';
 import Payment from './Page/Payment';
 import SingleProductPage from './Page/SingleProductPage';
 import { AllRoutes } from './Routes/AllRoutes';
+import { ThemeProvider, CSSReset } from "@chakra-ui/core";
+
 function App() {
   return (
     <div className="App">
