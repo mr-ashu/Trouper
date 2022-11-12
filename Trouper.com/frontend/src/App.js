70c4e89a469e557b414d29558e453151623c7989
Ashutosh Sharma <104469510+mr-ashu@users.noreply.github.com>
2022-11-12 07:25:21 +0530

    admin-intro

--- Trouper.com/frontend/src/App.js
+++ Trouper.com/frontend/src/App.js
@@ -11,7 +11,7 @@ function App() {
  
       <Navbar />
       <AllRoutes />
-      <SingleProductPage />
+    
  
  
     </div>
--- Trouper.com/frontend/src/Page/admin/AdminPage.jsx
+++ Trouper.com/frontend/src/Page/admin/AdminPage.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
+import { AdminIntro } from './adminIntro'
 
 export const AdminPage = () => {
   return (
     <div>
-      
-      AdminPage
-     
+    
+     <AdminIntro/>
 
 
     </div>
