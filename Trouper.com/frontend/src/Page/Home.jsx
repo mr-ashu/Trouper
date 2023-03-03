189901cd220892ced42184988bd598efc942435e
pravin0428 <mohite46199@gmail.com>
2023-03-03 12:29:19 +0530

    done:code cleanig and api fixing

--- Trouper.com/frontend/src/Page/Home.jsx
+++ Trouper.com/frontend/src/Page/Home.jsx
@@ -1,27 +1,24 @@
-import React from 'react'
-import { Slider } from './Home/Slider';
-import { Guide } from './Home/Guide';
-import { Talent } from './Home/Talent';
-import { Business } from './Home/Business';
-import { Freelancer } from './Home/Freelancer';
-import { MarketPlace } from './Home/MarketPlace';
-import { LogoMaker } from './Home/LogoMaker';
-import { Viewed } from './Home/Viewed';
-import { Services } from './Home/Services';
+import React from "react";
+import { Slider } from "./Home/Slider";
+import { Guide } from "./Home/Guide";
+import { Talent } from "./Home/Talent";
+import { Business } from "./Home/Business";
+import { Freelancer } from "./Home/Freelancer";
+import { MarketPlace } from "./Home/MarketPlace";
+import { LogoMaker } from "./Home/LogoMaker";
+import { Viewed } from "./Home/Viewed";
+import { Services } from "./Home/Services";
 export const Home = () => {
   return (
     <div>
-       
       <Talent />
-     
-       <Viewed />
-       <Services />
-       <Freelancer />
-       <MarketPlace /> 
-       <Business />
-       <LogoMaker />
-       <Guide />
-      
+      <Viewed />
+      <Services />
+      <Freelancer />
+      <MarketPlace />
+      <Business />
+      <LogoMaker />
+      <Guide />
     </div>
-  )
-}
+  );
+};
